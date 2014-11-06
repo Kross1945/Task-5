77a6acfafac9bccac124b8dc4ee7e41aa08a1fae
Kross1945 <kross1945@mail.ru>
2014-11-06 21:06:36 +0300

    Работа с формами

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/MainForm.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiff = new System.Windows.Forms.Button();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.btnStepen = new System.Windows.Forms.Button();
+            this.btnSin = new System.Windows.Forms.Button();
+            this.btnCos = new System.Windows.Forms.Button();
+            this.btnFact = new System.Windows.Forms.Button();
+            this.btnSqrt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Первое число:";
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(96, 6);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstNumber.TabIndex = 0;
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(96, 29);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtSecondNumber.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Результат:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Второе число:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(96, 121);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 7;
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(99, 67);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(23, 23);
+            this.btnDiv.TabIndex = 5;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(70, 67);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(23, 23);
+            this.btnMul.TabIndex = 4;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiff
+            // 
+            this.btnDiff.Location = new System.Drawing.Point(41, 67);
+            this.btnDiff.Name = "btnDiff";
+            this.btnDiff.Size = new System.Drawing.Size(23, 23);
+            this.btnDiff.TabIndex = 3;
+            this.btnDiff.Text = "-";
+            this.btnDiff.UseVisualStyleBackColor = true;
+            this.btnDiff.Click += new System.EventHandler(this.btnDiff_Click);
+            // 
+            // btnSum
+            // 
+            this.btnSum.Location = new System.Drawing.Point(12, 67);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(23, 23);
+            this.btnSum.TabIndex = 2;
+            this.btnSum.Text = "+";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // btnStepen
+            // 
+            this.btnStepen.Location = new System.Drawing.Point(128, 67);
+            this.btnStepen.Name = "btnStepen";
+            this.btnStepen.Size = new System.Drawing.Size(36, 23);
+            this.btnStepen.TabIndex = 6;
+            this.btnStepen.Text = "a^b";
+            this.btnStepen.UseVisualStyleBackColor = true;
+            this.btnStepen.Click += new System.EventHandler(this.btnStepen_Click);
+            // 
+            // btnSin
+            // 
+            this.btnSin.Location = new System.Drawing.Point(170, 67);
+            this.btnSin.Name = "btnSin";
+            this.btnSin.Size = new System.Drawing.Size(34, 23);
+            this.btnSin.TabIndex = 5;
+            this.btnSin.Text = "Sin";
+            this.btnSin.UseVisualStyleBackColor = true;
+            this.btnSin.Click += new System.EventHandler(this.btnSin_Click);
+            // 
+            // btnCos
+            // 
+            this.btnCos.Location = new System.Drawing.Point(210, 67);
+            this.btnCos.Name = "btnCos";
+            this.btnCos.Size = new System.Drawing.Size(34, 23);
+            this.btnCos.TabIndex = 5;
+            this.btnCos.Text = "Cos";
+            this.btnCos.UseVisualStyleBackColor = true;
+            this.btnCos.Click += new System.EventHandler(this.btnCos_Click);
+            // 
+            // btnFact
+            // 
+            this.btnFact.Location = new System.Drawing.Point(250, 67);
+            this.btnFact.Name = "btnFact";
+            this.btnFact.Size = new System.Drawing.Size(34, 23);
+            this.btnFact.TabIndex = 5;
+            this.btnFact.Text = "n!";
+            this.btnFact.UseVisualStyleBackColor = true;
+            this.btnFact.Click += new System.EventHandler(this.btnFct_Click);
+            // 
+            // btnSqrt
+            // 
+            this.btnSqrt.Location = new System.Drawing.Point(232, 96);
+            this.btnSqrt.Name = "btnSqrt";
+            this.btnSqrt.Size = new System.Drawing.Size(52, 23);
+            this.btnSqrt.TabIndex = 5;
+            this.btnSqrt.Text = "Sqrt^b";
+            this.btnSqrt.UseVisualStyleBackColor = true;
+            this.btnSqrt.Click += new System.EventHandler(this.btnSqrtB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 197);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.btnDiff);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnStepen);
+            this.Controls.Add(this.btnSqrt);
+            this.Controls.Add(this.btnFact);
+            this.Controls.Add(this.btnCos);
+            this.Controls.Add(this.btnSin);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiff;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Button btnStepen;
+        private System.Windows.Forms.Button btnSin;
+        private System.Windows.Forms.Button btnCos;
+        private System.Windows.Forms.Button btnFact;
+        private System.Windows.Forms.Button btnSqrt;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/MainForm.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+            
+            int.TryParse(txtFirstNumber.Text, out a);
+            int.TryParse(txtSecondNumber.Text, out b);
+
+            int sum = a + b;
+
+            txtResult.Text = sum.ToString();
+        }
+
+        private void btnDiff_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+
+            int.TryParse(txtFirstNumber.Text, out a);
+            int.TryParse(txtSecondNumber.Text, out b);
+
+            int diff = a - b;
+
+
+            txtResult.Text = diff.ToString();
+
+
+
+            
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+
+            int.TryParse(txtFirstNumber.Text, out a);
+            int.TryParse(txtSecondNumber.Text, out b);
+
+            int mul = a * b;
+
+            txtResult.Text = mul.ToString();
+
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+
+            int.TryParse(txtFirstNumber.Text, out a);
+            int.TryParse(txtSecondNumber.Text, out b);
+
+            int div = a / b;
+
+
+            txtResult.Text = div.ToString();
+        }
+    
+         private void btnStepen_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+
+            int.TryParse(txtFirstNumber.Text, out a);
+            int.TryParse(txtSecondNumber.Text, out b);
+
+            double stepen = Math.Pow(a, b);
+
+            txtResult.Text = stepen.ToString();
+            {
+            }
+
+        }
+
+         private void btnSin_Click(object sender, EventArgs e)
+         {
+             double a = 0;
+             
+             double.TryParse(txtFirstNumber.Text, out a);
+             
+             double sin = Math.Sin(a);
+
+             txtResult.Text = sin.ToString();
+
+
+
+         }
+
+         private void btnCos_Click(object sender, EventArgs e)
+         {
+             double a = 0;
+             
+             double.TryParse(txtFirstNumber.Text, out a);
+             
+             double cos = Math.Cos(a);
+
+             txtResult.Text = cos.ToString();
+         }
+         
+         private void btnFct_Click(object sender, EventArgs e)
+         {
+
+            int a = 0;
+          
+            int.TryParse(txtFirstNumber.Text, out a);
+
+            int fact = Factorial(a);
+            txtResult.Text = fact.ToString();
+        }
+         private static int Factorial(int n)
+         {
+             int res = 1;
+             for (int i = 2; i <= n; i++)
+             {
+                 res *= i;
+             }
+             return res;
+             
+         }
+
+        private void btnSqrtB_Click(object sender, EventArgs e)
+        {
+            double a = 0;
+            double b = 0;
+
+            double.TryParse(txtFirstNumber.Text, out a);
+            double.TryParse(txtSecondNumber.Text, out b);
+
+            double c = 1 / b;
+            double radical = Math.Pow(a, c);
+            txtResult.Text = radical.ToString();
+
+        }
+    }
+}
